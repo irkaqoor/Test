142d5a9ec9270dd3d5710983dce77933dcd37224
irkaqoor <irkaqoor@gmail.com>
2017-05-02 00:09:55 +0300

    test
    test

---
+++ src/algoritm.java
@@ -0,0 +1,26 @@
+
+public class algoritm {
+
+	public static void main(String[] args) {
+
+		int myArray[] = {0,0,1,1,0,0,0,1,1,1,1,0,1,1,1,1,1,1,0};
+
+		System.out.println(search(myArray));
+	}
+
+	public static int search(int[] array){
+		int maxCount = 0; int countTemp = 0;
+		for(int i = 0; i < array.length; i++) {
+			if (array[i] == 1){
+				countTemp++;
+			} else {
+				if(countTemp > maxCount){
+					maxCount = countTemp;
+				}
+				countTemp = 0;
+			}
+		}
+		return maxCount;
+	}
+}
+
